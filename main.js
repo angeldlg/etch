93ce55bc1031de5d2105cee988d24bf175c30f2f
angeldlg <angeldl2401@gmail.com>
2023-10-07 23:20:46 -0500

    optimized etch (before it was really laggy)

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".container");
 const clear = document.querySelector(".clear");
 const slider = document.querySelector("#slider");
 const gridValue = document.querySelector("#gridValue");
-let gridSize = 16
+let gridSize = 16;
 gridValue.textContent = `${slider.value} x ${slider.value}`;
 
 function makeGrid(size) {
@@ -12,40 +12,41 @@ function makeGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const grid = document.createElement("div");
     container.appendChild(grid);
-
-    container.addEventListener("mousedown", color);
-    container.addEventListener("mousedown", () => {
-      container.addEventListener("mouseover", color);
-    });
-    window.addEventListener("mouseup", () => {
-      container.removeEventListener("mouseover", color);
-    });
     grid.classList.add("square");
   }
 }
+
+container.addEventListener("mousedown", color);
+container.addEventListener("mousedown", () => {
+  container.addEventListener("mouseover", color);
+});
+window.addEventListener("mouseup", () => {
+  container.removeEventListener("mouseover", color);
+});
+
 function color(e) {
   e.target.style.backgroundColor = "black";
 }
-function erase() {
-    container.innerHTML = ''
-    makeGrid(gridSize)
-//   const gridSquares = document.querySelectorAll(".square");
-
-//   gridSquares.forEach((square) => {
-//     square.removeAttribute("style");
-//   });
+function remake() {
+  container.innerHTML = "";
+  makeGrid(gridSize);
 }
+function decolor() {
+  const gridSquares = document.querySelectorAll(".square");
 
+  gridSquares.forEach((square) => {
+    square.removeAttribute("style");
+  });
+}
 slider.addEventListener("input", (e) => {
   gridValue.value = `${e.target.value} x ${e.target.value}`;
 });
-
 slider.addEventListener("mouseup", () => {
   gridSize = slider.value;
-  makeGrid(gridSize);
-  erase();
+  remake();
+  //   makeGrid(gridSize);
 });
 
 makeGrid(gridSize);
 
-clear.addEventListener("click", erase);
+clear.addEventListener("click", decolor);
